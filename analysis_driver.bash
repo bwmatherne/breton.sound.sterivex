fa9b4e192a6d240cdf8ca0a46dbb1e31b3d520f7
Brian Matherne <bwmatherne@icloud.com>
2022-08-10 23:11:45 -0500

    Initial commit

---
+++ analysis_driver.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+############################################################################
+
+# Analysis Driver for Sterivex filter data using Riffomonas.org instructions
+# Settings are inabled for running on the LSU HPC
+
+############################################################################
+
+# 1. Download mothur with miniconda
+
+conda install -c bioconda mothur
+conda install -c bioconda/label/cf201901 mothur
