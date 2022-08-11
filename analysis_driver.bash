85aa9cc4eb7a0784f742aa3a827e7cc87f0d19dc
Brian Matherne <bwmatherne@icloud.com>
2022-08-11 09:17:42 -0500

    Added mothur code

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -11,3 +11,12 @@
 
 conda install -c bioconda mothur
 conda install -c bioconda/label/cf201901 mothur
+
+#2. Create make file
+make.file(inputdir=data/raw, type=gz, prefix=stability, numcols=4)
+make.contigs(file=stability,  oligos=barcode.oligos, checkorient=t, inputdir=data/raw, outputdir=data/mothur/, processors=20)
+
+
+Undetermined_S0_L001_I1_001.fastq.gz
+metadata.tsv                       Undetermined_S0_L001_R1_001.fastq.gz
+mothur.1660174282.logfile          Undetermined_S0_L001_R2_001.fastq.gz
