c5f2d98311a7218e590f347528543ebbea15360c
Brian Matherne <bwmatherne@icloud.com>
2022-08-15 12:42:31 -0500

    added filter step

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -8,7 +8,7 @@
 ############################################################################
 
 #Demultiplex and denoise each Illumina run
-# Note - R settings for dad2 are foudn in the seq1 code script
+# Note - R settings for dada2 are found in the seq1 code script
 
 qsub code/seq1.pbs
 qsub code/seq2.pbs
@@ -17,3 +17,8 @@ qsub code/seq4.pbs
 
 # Combine output files from each run and make a single table, rept seqs, and tree files
 qsub code/combine_table.pbs
+
+# Taxonomic assignment based on classifier computed in Spring 2022
+qsub code/trainset.pbs
+
+# Run script to remove mitochondria, chloroplasts, and sequences with no phyla assignement
