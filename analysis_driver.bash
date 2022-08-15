2881b9d6f4e236ef519667cf08f5216550e7c2ca
Brian Matherne <bwmatherne@icloud.com>
2022-08-15 11:47:16 -0500

    Began adding scripts used for analyses

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -6,3 +6,11 @@
 # Settings are inabled for running on the LSU HPC
 
 ############################################################################
+
+#Demultiplex and denoise each Illumina run
+# Note - R settings for dad2 are foudn in the seq1 code script
+
+qsub code/seq1.pbs
+qsub code/seq2.pbs
+qsub code/seq3.pbs
+qsub code/seq4.pbs
