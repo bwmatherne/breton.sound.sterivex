a2e9c2524329288a6d949b4c54ce3d46babcfb87
Brian Matherne <bwmatherne@icloud.com>
2022-08-16 08:38:49 -0500

    save

--- code/bs_metadata.R
+++ code/bs_metadata.R
@@ -14,19 +14,16 @@ library(tidyverse)
 # clear R's brain
 rm(list = ls())
 
-seq3 <- read_tsv('data/references/metadata.seq123.tsv')
-seq4 <- read_tsv('data/references/metadata.seq4.tsv')
-
-join <- full_join(seq3, seq4)
+full <- read_tsv('data/raw/combined.metadata.tsv')
 
 # Remove NA values from table for qiime
-df <- as.data.frame(join)
+df <- as.data.frame(full)
 df <- sapply(df, as.character)
 df[is.na(df)] <- " "
 df <- as.data.frame(df)
 
 #Select only Diversion Event Samples
-diversion <- df %>% filter(Event == 'Diversion' | Event == 'categorical')
+bs <- df %>% filter(Site == 'BS')
 
 
-write_tsv(diversion,'data/references/diversion.metadata.tsv')
+write_tsv(bs,'data/process/bs.metadata.tsv')
