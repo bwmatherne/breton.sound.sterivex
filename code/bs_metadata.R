e62d15fbdd60630f16788b31a59d22e46304c140
Brian Matherne <bwmatherne@icloud.com>
2022-08-16 10:12:25 -0500

    Corrected issue that removed second row of metadata used in qiime2

--- code/bs_metadata.R
+++ code/bs_metadata.R
@@ -23,7 +23,7 @@ df[is.na(df)] <- " "
 df <- as.data.frame(df)
 
 #Select only Diversion Event Samples
-bs <- df %>% filter(Site == 'BS')
+bs <- df %>% filter(Site == 'BS' | Site == 'categorical')
 
 
 write_tsv(bs,'data/process/bs.metadata.tsv')
