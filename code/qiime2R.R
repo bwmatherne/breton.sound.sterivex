8b4f66b091ed63278f0a45857b221bd1bf1d9ce4
Brian Matherne <bwmatherne@icloud.com>
2022-08-24 11:54:28 -0500

    Determined that the wrapper function is adequate

--- code/qiime2R.R
+++ code/qiime2R.R
@@ -9,33 +9,18 @@ devtools::install_github("jbisanz/qiime2R")
 
 
 library(qiime2R)
+library(phyloseq)
 
-# Importing ASVs abundance file
-ASVs <- read_qza("data/process/bs.table.qza")
-
-#Importing metadata
-metadata <- read_q2metadata("data/process/bs.metadata.tsv")
-metadata$Month  <- as.factor(metadata$Month)
-metadata$Month <- fct_relevel(metadata$Month, c("1","2","3","4","5","6","8","9","10")) # Set month order for down stream plotting
-
-# Importing tree
-tree <- read_qza("data/process/rooted-tree.qza")
-# Importing taxonomy
-taxonomy <- read_qza("data/process/taxonomy.qza")
-taxonomy <- parse_taxonomy(taxonomy$data)
 
 # Create phyloseq object using wrapper
 physeq<-qza_to_phyloseq(
   features="data/process/bs.table.qza",
   tree="data/process/rooted-tree.qza",
   taxonomy="data/process/taxonomy.qza",
-  metadata = "data/process/bs.metadata.tsv"
+  metadata = "data/process/sal.metadata.tsv"
 )
-physeq
+physeq #View summary of object
 
-# Creating phyloseq object based on Madison example
-phy <- phyloseq(
-  otu_table(ASVs$data, taxa_are_rows = T),
-  phy_tree(tree$data),
-  tax_table(taxonomy),
-  sample_data(metadata))
+saveRDS(physeq, "data/phyloseq/phyloseq.RDS") #Save the object for use later
+
+
